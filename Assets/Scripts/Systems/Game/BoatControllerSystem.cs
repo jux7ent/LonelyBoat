e6e3df93691907fd68e1a0aaf9efa2ff56e9910a
Egor <jux7ent@yandex.ru>
2020-10-24 12:20:10 +0300

    improved boat movement

--- Assets/Scripts/Systems/Game/BoatControllerSystem.cs
+++ Assets/Scripts/Systems/Game/BoatControllerSystem.cs
@@ -19,36 +19,42 @@ public class BoatControllerSystem : GameSystem, IIniting, IRunning {
 
     [SerializeField] [BoxGroup("Boat movement")] private float viscosityCoeff = 0.1f;
     [SerializeField] [BoxGroup("Boat movement")] private Transform waterLevelTransform;
+    [SerializeField] private float test = 1f;
 
     private Vector3 prevRightPaddleForcePointPos;
     private Vector3 prevLeftPaddleForcePointPos;
+    private Vector3 prevBoatPosition;
     
     void IIniting.OnInit() {
-        
     }
 
     void IRunning.OnRun() {
         ApplyForceFromPaddle(rightPart, ref prevRightPaddleForcePointPos);
         ApplyForceFromPaddle(leftPart, ref prevLeftPaddleForcePointPos);
+
+        prevBoatPosition = boatRigidbody.transform.position;
     }
 
     private void ApplyForceFromPaddle(ForcePointsForBoat forcePointsForBoat, ref Vector3 prevPaddlePos) {
         float divingCoeff = waterLevelTransform.position.y - forcePointsForBoat.paddleForcePositionTranform.position.y;
         if (divingCoeff < 0) divingCoeff = 0;
+        divingCoeff *= divingCoeff;
+        
+        Vector3 currPaddleForcePos = boatRigidbody.transform.InverseTransformPoint(forcePointsForBoat.paddleForcePositionTranform.position);
+        Vector3 tempDeltaForVelocity = test*(currPaddleForcePos - prevPaddlePos);
+        Vector3 tempDeltaForVelocityBoat = boatRigidbody.transform.position - prevBoatPosition;
+        float paddleVelocity = (tempDeltaForVelocity + tempDeltaForVelocityBoat).sqrMagnitude;
 
-        Vector3 tempDeltaForVelocity = forcePointsForBoat.paddleForcePositionTranform.position - prevPaddlePos;
-        float paddleVelocity = tempDeltaForVelocity.sqrMagnitude;
-
-        Vector3 forceDirection =
-            Vector3.Cross(-tempDeltaForVelocity,
+        Vector3 forceDirection = -boatRigidbody.transform.TransformDirection(tempDeltaForVelocity) - tempDeltaForVelocityBoat;
+           /* Vector3.Cross(-tempDeltaForVelocity + tempDeltaForVelocityBoat,
                 forcePointsForBoat.forcePositionTransform.position -
-                forcePointsForBoat.paddleForcePositionTranform.position).normalized;
+                forcePointsForBoat.paddleForcePositionTranform.position).normalized;*/
 
-        Debug.DrawRay(forcePointsForBoat.forcePositionTransform.position, tempDeltaForVelocity * 10f, Color.magenta);
-        Debug.DrawRay(forcePointsForBoat.forcePositionTransform.position, forcePointsForBoat.paddleForcePositionTranform.position - forcePointsForBoat.forcePositionTransform.position, Color.green);
-        Debug.DrawRay(forcePointsForBoat.forcePositionTransform.position, forceDirection, Color.red);
+      //  Debug.DrawRay(forcePointsForBoat.forcePositionTransform.position, (-tempDeltaForVelocity + tempDeltaForVelocityBoat) * 10f, Color.magenta);
+     //   Debug.DrawRay(forcePointsForBoat.forcePositionTransform.position, forcePointsForBoat.paddleForcePositionTranform.position - forcePointsForBoat.forcePositionTransform.position, Color.green);
+        Debug.DrawRay(forcePointsForBoat.forcePositionTransform.position, forceDirection * 100f, Color.red);
 
-        prevPaddlePos = forcePointsForBoat.paddleForcePositionTranform.position;
+        prevPaddlePos = currPaddleForcePos;
         
         boatRigidbody.AddForceAtPosition(viscosityCoeff * paddleVelocity * divingCoeff * forceDirection, forcePointsForBoat.forcePositionTransform.position);
     }
--- Assets/Scripts/UIScreens/Game/GameUIScreen.cs
+++ Assets/Scripts/UIScreens/Game/GameUIScreen.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using Kuhpik;
 using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameUIScreen : UIScreen {
     [SerializeField] [BoxGroup("Joysticks")] private Joystick leftJoystick;
     [SerializeField] [BoxGroup("Joysticks")] private Joystick rightJoystick;
 
+    [SerializeField] [BoxGroup("Buttons")] private Button restartButton;
+
     public Joystick LeftJoystick => leftJoystick;
     public Joystick RightJoystick => rightJoystick;
+
+    public override void Subscribe() {
+        base.Subscribe();
+        
+        restartButton.onClick.AddListener(() => {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        });
+    }
 }
