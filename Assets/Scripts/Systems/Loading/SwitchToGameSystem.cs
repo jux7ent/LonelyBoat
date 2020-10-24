355f80e7ad8025938300d62156c177720cd0884e
Egor <jux7ent@yandex.ru>
2020-10-24 06:57:26 +0300

    added joystick controller

---
+++ Assets/Scripts/Systems/Loading/SwitchToGameSystem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Kuhpik;
+using UnityEngine;
+
+public class SwitchToGameSystem : GameSystem, IIniting {
+    void IIniting.OnInit() {
+        Bootstrap.ChangeGameState(EGamestate.Game);
+    }
+}
--- Assets/Scripts/UIScreens/Game/GameUIScreen.cs
+++ Assets/Scripts/UIScreens/Game/GameUIScreen.cs
@@ -1,7 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Kuhpik;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class GameUIScreen : UIScreen {
+    [SerializeField] [BoxGroup("Joysticks")] private Joystick leftJoystick;
+    [SerializeField] [BoxGroup("Joysticks")] private Joystick rightJoystick;
+
+    public Joystick LeftJoystick => leftJoystick;
+    public Joystick RightJoystick => rightJoystick;
 }
