f06557053a2431c6254b9b5a1b9fd21f8fcc1306
Egor <jux7ent@yandex.ru>
2020-10-24 07:52:04 +0300

    added paddles controller

--- Assets/Scripts/Systems/Game/PaddleControllerSystem.cs
+++ Assets/Scripts/Systems/Game/PaddleControllerSystem.cs
@@ -5,14 +5,38 @@ using NaughtyAttributes;
 using UnityEngine;
 
 public class PaddleControllerSystem : GameSystemWithScreen<GameUIScreen>, IIniting, IRunning {
+    // x < 0 -> down; y > 0 -> forward
     [SerializeField] [BoxGroup("Paddles")] private Transform leftPaddleTransform;
     [SerializeField] [BoxGroup("Paddles")] private Transform rightPaddleTransform;
-    
+
+    [SerializeField] [BoxGroup("Paddles movement")] private float joystickMultiplier = 0.2f;
+    [SerializeField] [BoxGroup("Paddles movement")] private float lerpCoeffPaddlesMovement = 0.125f;
+
+    private Vector2 cachedJoystickDirection;
+    private float cachedAnglesMultiplier;
+
     void IIniting.OnInit() {
-        
+        cachedAnglesMultiplier = 360f * joystickMultiplier;
     }
 
     void IRunning.OnRun() {
-        print(screen.LeftJoystick.Direction);
+        if (Input.GetMouseButton(0)) {
+            PaddleMovement(rightPaddleTransform, screen.RightJoystick.Direction, true);
+            PaddleMovement(leftPaddleTransform, screen.LeftJoystick.Direction);
+        }
+    }
+
+    private void PaddleMovement(Transform paddleTransform, Vector2 joystickDirection, bool incorrectTopBottom=false) {
+        Vector3 angles = paddleTransform.localEulerAngles;
+        
+        if (incorrectTopBottom) {
+            joystickDirection.y *= -1;
+        }
+        
+        float temp = joystickDirection.x;
+        joystickDirection.x = Mathf.LerpAngle(angles.x, joystickDirection.y * cachedAnglesMultiplier, lerpCoeffPaddlesMovement);
+        joystickDirection.y = Mathf.LerpAngle(angles.y,temp * cachedAnglesMultiplier, lerpCoeffPaddlesMovement);
+
+        paddleTransform.localEulerAngles = joystickDirection;
     }
 }
