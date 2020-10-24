880e11d985010e5723829133ca685500ae3f5786
Egor <jux7ent@yandex.ru>
2020-10-24 07:18:40 +0300

    setup paddle

---
+++ Assets/Scripts/Systems/Game/PaddleControllerSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Kuhpik;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class PaddleControllerSystem : GameSystemWithScreen<GameUIScreen>, IIniting, IRunning {
+    [SerializeField] [BoxGroup("Paddles")] private Transform leftPaddleTransform;
+    [SerializeField] [BoxGroup("Paddles")] private Transform rightPaddleTransform;
+    
+    void IIniting.OnInit() {
+        
+    }
+
+    void IRunning.OnRun() {
+        print(screen.LeftJoystick.Direction);
+    }
+}
