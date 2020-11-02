4b5ac8e52b755901661cd91ba4c9620631f5da65
Egor <jux7ent@yandex.ru>
2020-11-02 07:35:47 +0300

    added rocks and losing for it

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants {
+    public static class Tags {
+        public const string Rock = "Rock";
+    }
+}
---
+++ Assets/Scripts/Systems/Game/FinishCheckerSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Kuhpik;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishCheckerSystem : GameSystem, IIniting {
+    [SerializeField] private CollisionListener boatCollisionListener;
+    
+    private bool systemInited = false;
+    
+    void IIniting.OnInit() {
+        if (!systemInited) {
+            boatCollisionListener.CollisionEnterEvent += CollisionEnterHandler;
+            systemInited = true;
+        }
+    }
+
+    private void CollisionEnterHandler(Transform other) {
+        if (other.CompareTag(Constants.Tags.Rock)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
